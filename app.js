0d44c73227e01fb7e1ba1bd70b8d203448d54f6d
CoolShivas <shivas2710cool00@gmail.com>
2025-08-03 13:37:59 +0530

    Posted the data with the help of POSTMAN post method.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(5000, () => {
   console.log("Server running at 5000");
   // // Output on Terminal as :- Server running at 5000
@@ -14,4 +16,29 @@ app.listen(5000, () => {
 app.post("/post", async (request, response) => {
   // // // This is the basic structure of making an apies;
   console.log(request.body);
+  // // // Now, open the postman and set it to POSTMAN;
+  // // // Set the POST =>  http://localhost:5000/post
+  // // // Set the Headers =>  Key => Content-Type and Value => application/json
+  // // // Set the Body => row
+  // // // Write the data on body as => {
+  // // //     "username" : "shiva khateek chouhan"
+  // // // }
+  // // // Hit the Send button of POSTMAN;
+  // // // We will get error i.e.,=>
+  // // //     <!DOCTYPE html>
+  // // // <html lang="en">
+
+  // // // <head>
+  // // // 	<meta charset="utf-8">
+  // // // 	<title>Error</title>
+  // // // </head>
+
+  // // // <body>
+  // // // 	<pre>Cannot POST /post</pre>
+  // // // </body>
+
+  // // // </html>
+  // // // We will get Output on Terminal as => Undefined
+  // // // To resolve this error use => app.use(express.json());
+  // // // We will get Output on Terminal as => { username: 'shiva khateek chouhan' }
 });
