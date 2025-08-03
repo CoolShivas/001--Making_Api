156c2ee94e6cdc068823ff70571df16d1119b1bd
CoolShivas <shivas2710cool00@gmail.com>
2025-08-03 13:08:44 +0530

    Created the server 5000.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // // // Starting of formation or making of api's;
+
+const express = require("express");
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server running at 5000");
+  // // Output on Terminal as :- Server running at 5000
+});
