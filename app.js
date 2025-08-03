670d95646e576c73f550979e543dcb4ec5f680c3
CoolShivas <shivas2710cool00@gmail.com>
2025-08-03 13:21:11 +0530

    Created the basic structure of post api.

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ app.listen(5000, () => {
   console.log("Server running at 5000");
   // // Output on Terminal as :- Server running at 5000
 });
+
+// // // USING Post method to make an api's with the help of POSTMAN;
+
+app.post("/post", async (request, response) => {
+  // // // This is the basic structure of making an apies;
+  console.log(request.body);
+});
